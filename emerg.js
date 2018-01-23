933b83238bb44255e26b3a970c6db9151ae58bb4
Gilles Bouthenot <misc@atomas.com>
2018-01-23 15:38:12 +0100

    add LZ-string

--- emerg.js
+++ emerg.js
@@ -1,4 +1,4 @@
-/* global base64js */
+/* global base64js LZString */
 /* eslint-disable no-console,no-shadow,no-lonely-if */
 
 const iv1 = new Uint8Array([0, 1, 10, 13, 15, 16, 254, 255, 9, 10, 11, 12, 13, 14, 15, 16]);
@@ -7,7 +7,8 @@ const iv1 = new Uint8Array([0, 1, 10, 13, 15, 16, 254, 255, 9, 10, 11, 12, 13, 1
 const encryptText = async (plainText, password) => {
   // const iv1 = crypto.getRandomValues(new Uint8Array(16));
   const alg = { name: 'AES-CBC', iv: iv1 };
-  const ptUtf8 = new TextEncoder().encode(plainText);
+  // const ptUtf8 = new TextEncoder().encode(plainText);
+  const ptUtf8 = LZString.compressToUint8Array(plainText);
   console.log(`bytelength=${ptUtf8.byteLength}`);
   const pwUtf8 = new TextEncoder().encode(password);
   const pwHash = await crypto.subtle.digest('SHA-256', pwUtf8);
@@ -20,7 +21,8 @@ const decryptText = async (cryptedbytes, password) => {
   const pwUtf8 = new TextEncoder().encode(password);
   const pwHash = await crypto.subtle.digest('SHA-256', pwUtf8);
   const key = await crypto.subtle.importKey('raw', pwHash, alg, false, ['decrypt']);
-  return crypto.subtle.decrypt(alg, key, cryptedbytes);
+  const decrypted = await crypto.subtle.decrypt(alg, key, cryptedbytes);
+  return LZString.decompressFromUint8Array(new Uint8Array(decrypted));
 };
 
 
@@ -60,7 +62,7 @@ const decryptAction = () => {
 
   const a = decryptText(cryptedbytes, password);
   a.then((_) => {
-    document.getElementById('decryptzone').value = new TextDecoder().decode(_);
+    document.getElementById('decryptzone').value = _;
   });
 };
 
