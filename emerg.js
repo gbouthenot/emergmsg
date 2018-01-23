4453683a4c4252866f7e6c172882f9c1fb9eab8b
Gilles Bouthenot <misc@atomas.com>
2018-01-23 16:48:09 +0100

    remove clutter

--- emerg.js
+++ emerg.js
@@ -10,50 +10,32 @@ const concatTA = (a, b) => {
 };
 
 // https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/encrypt
-const encryptText = async (plainText, password) => {
+const encryptText = async (pt, pw) => {
   const alg = { name: 'AES-CBC', iv: crypto.getRandomValues(new Uint8Array(16)) };
-  // const ptUtf8 = new TextEncoder().encode(plainText);
-  const ptUtf8 = LZString.compressToUint8Array(plainText);
-  console.log(`bytelength=${ptUtf8.byteLength}`);
-  const pwUtf8 = new TextEncoder().encode(password);
-  const pwHash = await crypto.subtle.digest('SHA-256', pwUtf8);
+  const ptbin = LZString.compressToUint8Array(pt);
+  const pwbin = new TextEncoder().encode(pw);
+  const pwHash = await crypto.subtle.digest('SHA-256', pwbin);
   const key = await crypto.subtle.importKey('raw', pwHash, alg, false, ['encrypt']);
-  const crypted = await crypto.subtle.encrypt(alg, key, ptUtf8);
+  const crypted = await crypto.subtle.encrypt(alg, key, ptbin);
   return concatTA(alg.iv, new Uint8Array(crypted));
 };
 
-const decryptText = async (cryptedbytes, password) => {
-  const alg = { name: 'AES-CBC', iv: cryptedbytes.slice(0, 16) };
-  const pwUtf8 = new TextEncoder().encode(password);
-  const pwHash = await crypto.subtle.digest('SHA-256', pwUtf8);
+const decryptText = async (cr, pw) => {
+  const alg = { name: 'AES-CBC', iv: cr.slice(0, 16) };
+  const pwbin = new TextEncoder().encode(pw);
+  const pwHash = await crypto.subtle.digest('SHA-256', pwbin);
   const key = await crypto.subtle.importKey('raw', pwHash, alg, false, ['decrypt']);
-  const decrypted = await crypto.subtle.decrypt(alg, key, cryptedbytes.slice(16));
+  const decrypted = await crypto.subtle.decrypt(alg, key, cr.slice(16));
   return LZString.decompressFromUint8Array(new Uint8Array(decrypted));
 };
 
 
 const password = 'mypassword';
 
-/*
-const a = encryptText(document.getElementById('clearzone').value, password);
-a.then((_) => {
-  const crypted64 = base64js.fromByteArray(new Uint8Array(_));
-  document.getElementById('cryptzone').value = crypted64;
-  const cryptedbytes = base64js.toByteArray(crypted64);
-  return decryptText(cryptedbytes, password);
-}).then((_) => {
-  document.getElementById('decryptzone').value = new TextDecoder().decode(_);
-  return _;
-});
-*/
-
 const cryptAction = () => {
-  console.log(`clearzone.length=${document.getElementById('clearzone').value.length}`);
   const a = encryptText(document.getElementById('clearzone').value, password);
   a.then((_) => {
-    console.log(`crypted.length=${_.byteLength}`);
     const crypted64 = base64js.fromByteArray(new Uint8Array(_));
-    console.log(`crypted64.length=${crypted64.length}`);
     let formatted = '';
     for (let i = 0; i < crypted64.length; i += 120) {
       formatted += (i ? '\n' : '') + crypted64.slice(i, i + 120);
